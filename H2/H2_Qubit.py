5f09e7b1a642c27e1a6689f192f7ec0f8a1b066e
Mario_Chiaparini <45772937+MarioChiaparini@users.noreply.github.com>
2023-01-05 14:40:10 -0300

    Add files via upload

--- H2/H2_Qubit.py
+++ H2/H2_Qubit.py
@@ -13,13 +13,43 @@ from qiskit.circuit import Parameter, QuantumCircuit, QuantumRegister
 
 Tbackend = Aer.get_backend('statevector_simulator')
 
+#loading the molecule
 H2 = "H .0 .0 .0; H .0 .0 0.739"
-driver = PySCFDriver(atom=H2)
+driver = PySCFDriver(atom=H2,  
+                     unit=UnitsType.ANGSTROM,
+                     basis='sto3g')
 qmolecule = driver.run()
-
+#build the eletronic structure problem 
 problem = ElectronicStructureProblem(driver)
+#Generate the second-quantized operators 
 second_q_ops = problem.second_q_ops()
+#Hamiltonian
 main_p = second_q_ops[0]
 
 mapper = JordanWignerMapper()
 converter = QubitConverter(mapper=mapper)
+
+#Fermionic operador mapped to qubits operators
+num_particles = (problem.grouped_property_transformed.num_alpha,
+                problem.grouped_property_transformed.num_beta)
+qoperador = converter.convert(main_p, num_particles=num_particles) 
+
+#Create a Hartree-Fock circuit
+num_particles = (problem.grouped_property_transformed.num_alpha,
+             problem.grouped_property_transformed.num_beta)
+
+num_spin_orbitals = 2 * problem.grouped_property_transformed.num_spin_orbitals
+init_state = HartreeFock(num_spin_orbitals, num_particles, converter)
+
+#Create dummy parametrized circuit
+theta = Parameter('a')
+n = qubit_op.num_qubits
+qc = QuantumCircuit(qubit_op.num_qubits)
+qc.rz(theta*0,0)
+ansatz = qc
+ansatz.compose(init_state, front=True, inplace=True)
+
+#Pass it through VQE
+algorithm = VQE(ansatz,quantum_instance=backend)
+result = algorithm.compute_minimum_eigenvalue(qubit_op).eigenvalue
+print(result)
